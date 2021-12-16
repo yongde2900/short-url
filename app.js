6024f0da3457a0dc96c53e882355d7d68688ba99
yongde2900 <com414141@gmail.com>
2021-12-16 22:31:12 +0800

    project init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+//handlebars
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+
+app.listen(PORT, () => {
+    console.log('server is listening')
+})
