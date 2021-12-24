782fe0021c819f2d335aa63b1b444e177e153035
yongde2900 <com414141@gmail.com>
2021-12-25 02:03:37 +0800

    feat: use bcrypt to compare passpord

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const LocalStrategy = require('passport-local').Strategy
 const User = require('../models/user')
 
@@ -14,7 +15,7 @@ module.exports = (app) => {
             let user = await User.findOne({ email }).exec()
             if (!user)
                 return done(null, false, { message: 'Incorrect email' })
-            if (user.password !== password)
+            if (!bcrypt.compareSync(password, user.password))
                 return done(null, false, { message: 'Incorrect password' })
             return done(null, user)
         }
